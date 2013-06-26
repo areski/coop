c2911cec4a5dde469057d5c35acf42655d715815
Areski Belaid <areski@gmail.com>
2013-06-26 18:29:11 +0200

    import mockup

---
+++ NoticiaProto/cooperante_2_0_hub_files/axurerp_pagespecificscript.js
@@ -0,0 +1,294 @@
+﻿for(var i = 0; i < 80; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+document.getElementById('u50_img').tabIndex = 0;
+
+u50.style.cursor = 'pointer';
+$axure.eventManager.click('u50', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('estadisticas.html');
+
+}
+});
+gv_vAlignTable['u51'] = 'center';document.getElementById('u52_img').tabIndex = 0;
+
+u52.style.cursor = 'pointer';
+$axure.eventManager.click('u52', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('amigos.html');
+
+}
+});
+gv_vAlignTable['u53'] = 'center';document.getElementById('u54_img').tabIndex = 0;
+
+u54.style.cursor = 'pointer';
+$axure.eventManager.click('u54', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('misMisiones.html');
+
+}
+});
+gv_vAlignTable['u55'] = 'center';document.getElementById('u56_img').tabIndex = 0;
+
+u56.style.cursor = 'pointer';
+$axure.eventManager.click('u56', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('donar.html');
+
+}
+});
+gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u58'] = 'top';document.getElementById('u59_img').tabIndex = 0;
+
+u59.style.cursor = 'pointer';
+$axure.eventManager.click('u59', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('donar.html');
+
+}
+});
+gv_vAlignTable['u20'] = 'center';
+u21.style.cursor = 'pointer';
+if (bIE) u21.attachEvent("onclick", ClickLinkToReferencePageu21);
+else u21.addEventListener("click", ClickLinkToReferencePageu21, true);
+function ClickLinkToReferencePageu21(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('noticia.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u21ann, "<img src='resources/images/newwindow.gif' id='u21PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u21PagePopup = document.getElementById('u21PagePopup');
+if (bIE) u21PagePopup.attachEvent("onclick", u21PagePopupHandler);
+else u21PagePopup.addEventListener("click", u21PagePopupHandler, true);
+
+function u21PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('noticia.html'));
+}
+gv_vAlignTable['u22'] = 'center';
+u23.style.cursor = 'pointer';
+if (bIE) u23.attachEvent("onclick", ClickLinkToReferencePageu23);
+else u23.addEventListener("click", ClickLinkToReferencePageu23, true);
+function ClickLinkToReferencePageu23(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u23ann, "<img src='resources/images/newwindow.gif' id='u23PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u23PagePopup = document.getElementById('u23PagePopup');
+if (bIE) u23PagePopup.attachEvent("onclick", u23PagePopupHandler);
+else u23PagePopup.addEventListener("click", u23PagePopupHandler, true);
+
+function u23PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('.html'));
+}
+gv_vAlignTable['u24'] = 'center';
+u25.style.cursor = 'pointer';
+if (bIE) u25.attachEvent("onclick", ClickLinkToReferencePageu25);
+else u25.addEventListener("click", ClickLinkToReferencePageu25, true);
+function ClickLinkToReferencePageu25(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('donar.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u25ann, "<img src='resources/images/newwindow.gif' id='u25PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u25PagePopup = document.getElementById('u25PagePopup');
+if (bIE) u25PagePopup.attachEvent("onclick", u25PagePopupHandler);
+else u25PagePopup.addEventListener("click", u25PagePopupHandler, true);
+
+function u25PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('donar.html'));
+}
+gv_vAlignTable['u26'] = 'center';
+u27.style.cursor = 'pointer';
+if (bIE) u27.attachEvent("onclick", ClickLinkToReferencePageu27);
+else u27.addEventListener("click", ClickLinkToReferencePageu27, true);
+function ClickLinkToReferencePageu27(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('misMisiones.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u27ann, "<img src='resources/images/newwindow.gif' id='u27PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u27PagePopup = document.getElementById('u27PagePopup');
+if (bIE) u27PagePopup.attachEvent("onclick", u27PagePopupHandler);
+else u27PagePopup.addEventListener("click", u27PagePopupHandler, true);
+
+function u27PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('misMisiones.html'));
+}
+gv_vAlignTable['u28'] = 'center';
+u29.style.cursor = 'pointer';
+if (bIE) u29.attachEvent("onclick", ClickLinkToReferencePageu29);
+else u29.addEventListener("click", ClickLinkToReferencePageu29, true);
+function ClickLinkToReferencePageu29(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('homepage.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u29ann, "<img src='resources/images/newwindow.gif' id='u29PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u29PagePopup = document.getElementById('u29PagePopup');
+if (bIE) u29PagePopup.attachEvent("onclick", u29PagePopupHandler);
+else u29PagePopup.addEventListener("click", u29PagePopupHandler, true);
+
+function u29PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('homepage.html'));
+}
+gv_vAlignTable['u60'] = 'center';document.getElementById('u61_img').tabIndex = 0;
+
+u61.style.cursor = 'pointer';
+$axure.eventManager.click('u61', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('misMisiones.html');
+
+}
+});
+gv_vAlignTable['u62'] = 'center';document.getElementById('u63_img').tabIndex = 0;
+
+u63.style.cursor = 'pointer';
+$axure.eventManager.click('u63', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('amigos.html');
+
+}
+});
+gv_vAlignTable['u64'] = 'center';document.getElementById('u65_img').tabIndex = 0;
+
+u65.style.cursor = 'pointer';
+$axure.eventManager.click('u65', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('estadisticas.html');
+
+}
+});
+gv_vAlignTable['u66'] = 'center';gv_vAlignTable['u68'] = 'center';document.getElementById('u69_img').tabIndex = 0;
+
+u69.style.cursor = 'pointer';
+$axure.eventManager.click('u69', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('homepage.html');
+
+}
+});
+gv_vAlignTable['u30'] = 'center';
+u31.style.cursor = 'pointer';
+if (bIE) u31.attachEvent("onclick", ClickLinkToReferencePageu31);
+else u31.addEventListener("click", ClickLinkToReferencePageu31, true);
+function ClickLinkToReferencePageu31(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('map.html');
+}
+
+x = 0;
+y = 56;
+InsertAfterBegin(u31ann, "<img src='resources/images/newwindow.gif' id='u31PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u31PagePopup = document.getElementById('u31PagePopup');
+if (bIE) u31PagePopup.attachEvent("onclick", u31PagePopupHandler);
+else u31PagePopup.addEventListener("click", u31PagePopupHandler, true);
+
+function u31PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('map.html'));
+}
+gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u70'] = 'center';gv_vAlignTable['u73'] = 'center';gv_vAlignTable['u75'] = 'center';gv_vAlignTable['u77'] = 'center';gv_vAlignTable['u79'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u48'] = 'center';gv_vAlignTable['u46'] = 'center';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u14'] = 'center';
+u15.style.cursor = 'pointer';
+if (bIE) u15.attachEvent("onclick", ClickLinkToReferencePageu15);
+else u15.addEventListener("click", ClickLinkToReferencePageu15, true);
+function ClickLinkToReferencePageu15(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('cooperante_2_0_hub.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u15ann, "<img src='resources/images/newwindow.gif' id='u15PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u15PagePopup = document.getElementById('u15PagePopup');
+if (bIE) u15PagePopup.attachEvent("onclick", u15PagePopupHandler);
+else u15PagePopup.addEventListener("click", u15PagePopupHandler, true);
+
+function u15PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('cooperante_2_0_hub.html'));
+}
+gv_vAlignTable['u16'] = 'center';
+u17.style.cursor = 'pointer';
+if (bIE) u17.attachEvent("onclick", ClickLinkToReferencePageu17);
+else u17.addEventListener("click", ClickLinkToReferencePageu17, true);
+function ClickLinkToReferencePageu17(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('amigos.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u17ann, "<img src='resources/images/newwindow.gif' id='u17PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u17PagePopup = document.getElementById('u17PagePopup');
+if (bIE) u17PagePopup.attachEvent("onclick", u17PagePopupHandler);
+else u17PagePopup.addEventListener("click", u17PagePopupHandler, true);
+
+function u17PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('amigos.html'));
+}
+gv_vAlignTable['u18'] = 'center';
+u19.style.cursor = 'pointer';
+if (bIE) u19.attachEvent("onclick", ClickLinkToReferencePageu19);
+else u19.addEventListener("click", ClickLinkToReferencePageu19, true);
+function ClickLinkToReferencePageu19(e)
+{
+    self.location.href=$axure.globalVariableProvider.getLinkUrl('estadisticas.html');
+}
+
+x = 0;
+y = 74;
+InsertAfterBegin(u19ann, "<img src='resources/images/newwindow.gif' id='u19PagePopup' style='cursor:pointer;position:absolute;z-index:500;left:" + x + ";top:" + y + "'>");
+
+var u19PagePopup = document.getElementById('u19PagePopup');
+if (bIE) u19PagePopup.attachEvent("onclick", u19PagePopupHandler);
+else u19PagePopup.addEventListener("click", u19PagePopupHandler, true);
+
+function u19PagePopupHandler(event)
+{
+    window.open($axure.globalVariableProvider.getLinkUrl('estadisticas.html'));
+}
---
+++ NoticiaProto/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l),_(c,m,e,f,g,n),_(c,o,e,f,g,p),_(c,q,e,f,g,r),_(c,s,e,f,g,t),_(c,u,e,f,g,v)]);}; 
+var b="rootNodes",c="pageName",d="map",e="type",f="Wireframe",g="url",h="map.html",i="homepage",j="homepage.html",k="cooperante 2.0 hub",l="cooperante_2_0_hub.html",m="estadisticas",n="estadisticas.html",o="amigos",p="amigos.html",q="misMisiones",r="misMisiones.html",s="donar",t="donar.html",u="noticia",v="noticia.html";
+return _creator();
+})();
